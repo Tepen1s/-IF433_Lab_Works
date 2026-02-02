7623ce3a8695eb8990a0a6913fcaca7644378baa
Tepen1s <stevenwan07@gmail.com>
2026-02-02 17:26:56 +0700

    GradingSystem.kt

--- src/oop_00000077033_StevenWan/week01/GradingSystem.kt
+++ src/oop_00000077033_StevenWan/week01/GradingSystem.kt
@@ -1,8 +1,8 @@
 package oop_00000077033_StevenWan.week01
 
 fun main () {
-    var name: String = "Jhon Thor"
-    var score: Int = 80
+    val name: String = "Jhon Thor"
+    val score: Int = 80
 
     println("Nama:  $name, Nilai:  $score")
 }
