1296a65068334bbff72000bdc549b19dd1fbf32f
Tepen1s <stevenwan07@gmail.com>
2026-02-02 22:47:44 +0700

    Add files via upload

--- src/oop_00000077033_StevenWan/week01/CircleCalculator.kt
+++ src/oop_00000077033_StevenWan/week01/CircleCalculator.kt
@@ -2,20 +2,17 @@ package oop_00000077033_StevenWan.week01
 
 fun main (args: Array<String>) {
 
-    var radius: Double = 7.0
-    var pi: Double = 3.14
+    val radius: Double = 7.0
+    val pi: Double = 3.14
 
-    var area: Double = pi * radius * radius
+    val area: Double = pi * radius * radius
 
-    println("Radius: " + radius + ", Area: " + area)
-
-    checkSize (area)
+    println("Radius: $radius, Area: $area")
+    println(checkSize(area))
 }
 
-fun checkSize(area: Double) {
-    if (area > 100) {
-        println("This is a Big Circle")
-    } else {
-        println("This is a Small Circle")
-    }
+fun checkSize(area: Double) = if (area > 100) {
+    "This is a Big Circle"
+} else {
+    "This is a Small Circle"
 }
